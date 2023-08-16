00c43a1ce822810b160a11ac1aec6d52c2569f61
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2023-08-16 11:22:05 +0530

    initial payroll

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,44 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String role;
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o){
+            return true;
+        }
+        if(!(o instanceof Employee)){
+            return false;
+        }
+        Employee employee =(Employee) o;
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
+                && Objects.equals(this.role, employee.role);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.id, this.name, this.role);
+    }
+
+    @Override
+    public String toString(){
+        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+    }
+
+}
